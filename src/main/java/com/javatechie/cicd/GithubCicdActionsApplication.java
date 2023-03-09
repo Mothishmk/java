6d463b72fb2ce620642bfd80089d4910dccb9d72
Mothishmk <123073644+Mothishmk@users.noreply.github.com>
2023-03-09 15:59:08 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,9 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Infosys Limited is an Indian multinational information technology company that provides business consulting, information technology and outsourcing services. The company was founded in Pune and is headquartered in Bangalore.[5] Infosys is the second-largest Indian IT company, after Tata Consultancy Services, by 2020 revenue figures, and the 602nd largest public company in the world, according to the Forbes Global 2000 ranking.[6]
-
-On 24 August 2021, Infosys became the fourth Indian company to reach $100 billion in market capitalization.";
+		return "welcome";
 	}
 
 //	echo "# github-actions-example" >> README.md

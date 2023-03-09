2a6521926cefdac5804ac587140a945933451e8c
Mothishmk <123073644+Mothishmk@users.noreply.github.com>
2023-03-09 15:04:03 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "WELCOME TO QUALITY AUTOMATION GROUP !";
+		return "FIRST DEMO PROJECT!";
 	}
 
 //	echo "# github-actions-example" >> README.md
